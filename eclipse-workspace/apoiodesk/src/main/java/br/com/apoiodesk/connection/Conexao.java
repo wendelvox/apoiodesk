f604acc4dc303e4564fa8ea1b8fa7b85a6ca1eb9
wendelvox <wendelvoxs@gmail.com>
2025-06-12 20:58:52 -0300

    add connection ao filter

--- eclipse-workspace/apoiodesk/src/main/java/br/com/apoiodesk/connection/Conexao.java
+++ eclipse-workspace/apoiodesk/src/main/java/br/com/apoiodesk/connection/Conexao.java
@@ -6,23 +6,37 @@ import java.sql.DriverManager;
 public class Conexao {
 	
 	private static Connection conn = null;
+	private static String url = "jdbc:postgresql://localhost:5432/sisdoc";
+	private static String user = "postgres";
+	private static String password = "123456";
+	
+	public Conexao() {
+		conectar();
+	}
+	
+	static {
+		conectar();
+	}
+	
+	public static Connection getConnection(){
+		return conn;
+	}
+
 	
 	//Metodo conexão com o banco
-	public static Connection getConnection() {
+	public static void conectar() {
 		try {
-			if (conn == null || conn.isClosed()) {
-				String url = "jdbc:postgresql://localhost:5432/sisdoc";
-				String user = "postgres";
-				String password = "123456";
+			if (conn == null || conn.isClosed()) {		
 				
-				Class.forName("org.postgresql.Driver");
+				Class.forName("org.postgresql.Driver");	
 				
 				conn = DriverManager.getConnection(url,user,password);
+				conn.setAutoCommit(false);
 			}
 		} catch (Exception e) {
 			throw new RuntimeException("Erro ao conectar o banco", e);
 		}
-		return conn;
+		
 	}
 	
 
--- eclipse-workspace/apoiodesk/src/main/java/br/com/apoiodesk/filtros/FilterAutenticacao.java
+++ eclipse-workspace/apoiodesk/src/main/java/br/com/apoiodesk/filtros/FilterAutenticacao.java
@@ -1,7 +1,10 @@
 package br.com.apoiodesk.filtros;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import br.com.apoiodesk.connection.Conexao;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -20,48 +23,65 @@ import jakarta.servlet.http.HttpSession;
 
 @WebFilter("/principal/*")
 public class FilterAutenticacao extends HttpFilter implements Filter {
-       
-    
-    public FilterAutenticacao() {
-        super();
-        
-    }
+
+	private static Connection connection;
+
+	public FilterAutenticacao() {
+		super();
+
+	}
 
 	/* Encerras os processos quando o servidor é parado */
 	public void destroy() {
-		
+
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	/* Intercepta as requisições e as repostas do sistema*/
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpSession session = req.getSession();
-		
-		String usuarioLogado = (String) session.getAttribute("usuario");
-		
-		String urlParaAutentica = req.getServletPath();
-		
-		/* Validar se está logado */
-		if(usuarioLogado == null &&
-				!urlParaAutentica.contains("/principal/ServletLogin")) {
-			
-			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutentica);
-			request.setAttribute("msg", "Por favor realize o login!");
-			redireciona.forward(request, response);
-			return;
-			
-		}else {
-			chain.doFilter(request, response);
+	/* Intercepta as requisições e as repostas do sistema */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		try {
+
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+
+			String usuarioLogado = (String) session.getAttribute("usuario");
+
+			String urlParaAutentica = req.getServletPath();
+
+			/* Validar se está logado */
+			if (usuarioLogado == null && !urlParaAutentica.contains("/principal/ServletLogin")) {
+
+				RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutentica);
+				request.setAttribute("msg", "Por favor realize o login!");
+				redireciona.forward(request, response);
+				return;
+
+			} else {
+				chain.doFilter(request, response);
+			}
+				connection.commit();
+		    } catch (Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
-		
-		
-		
 	}
 
 	/* Inicia os processos */
 	public void init(FilterConfig fConfig) throws ServletException {
-		
+
+		connection = Conexao.getConnection();
 	}
 
 }
